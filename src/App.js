6ea97ed0a3ede7a8407d20daac76abce092f8a5e
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-19 22:26:06 +0300

    Add Chao

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+// import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Chao from "./components/Chao";
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p> 
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  return React.createElement('div',{className:'App'},
-  React.createElement('header',{className:'App-header'},
-    React.createElement('img',{
-      className:"App-logo",
-      src: logo,
-      alt:"logo"
-    }),
-    React.createElement('p', null, 
-      'Edit ',
-      React.createElement('code', null, 'src/index.js'),
-      ' and save to reload.'
-    ),
-    React.createElement('a',{
-      className:"App-link",
-      href:"https://reactjs.org",
-      target:"_blank",
-      rel:"noopener noreferrer",
-    },' Learn React')
-  )
-);
+  return <Chao />;
 }
 
 export default App;
---
+++ src/components/Chao/index.js
@@ -0,0 +1,10 @@
+
+import { Component } from "react";
+
+class Chao extends Component {
+  render() {
+    return <h2>Hi! user!</h2>;
+  }
+}
+
+export default Chao; 
